38c88e99b3492979c1d34ac4e776fc9f85e99dd9
Gavin Look <GTLook@gmail.com>
2018-03-26 15:12:13 -0700

    Added data.js and product names

---
+++ JS/data.js
@@ -0,0 +1,87 @@
+let data = {
+
+  'breakfast' : {
+    'Jawsome Breakfast Scramble' : {
+      'image' : link,
+      'price' : 14,
+      'description' : text
+    },
+    'Mako own Burrito' : {
+      'image' : link,
+      'price' : 15,
+      'description' : text
+    },
+    'Grumpy Gills Grapefruit' : {
+      'image' : link,
+      'price' : 9,
+      'description' : text
+    },
+    'Buna-Buna-Buna bread' ; {
+      'image' : link,
+      'price' : 12,
+      'description' : text
+    }
+  },
+
+
+  'lunch' : {
+    'Fin-tastic Pizza' : {
+      'image' : 'https://www.biggerbolderbaking.com/wp-content/uploads/2016/01/IMG_9114-1024x682.jpg',
+      'price' : 13,
+      'description' : 'Pizza!'
+    },
+    'Left-Shark mac' : {
+      'image' : link,
+      'price' : 14,
+      'description' : text
+    },
+    'Great White Pot Pie' : {
+      'image' : link,
+      'price' : 17,
+      'description' : text
+    }
+  },
+
+
+  'dessert' : {
+    'Jaws Dropping Brownie' : {
+      'image' : link,
+      'price' : 9,
+      'description' : text
+    },
+    'Fin Mint Chocolate Cake' : {
+      'image' : link,
+      'price' : 11,
+      'description' : text
+    },
+    'Shark out of Water': {
+      'image' : 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/12/3/0/FN_SC-Hot-Chocolate-Toasted-Marshmallow_s4x3.jpg.rend.hgtvcom.966.725.suffix/1449177520596.jpeg',
+      'price' : 9,
+      'description' : 'hot chocolate and toasted marshmellows'
+    }
+  },
+
+
+  'drinks' : {
+    'Fishing for Donuts' : {
+      'image' : link,
+      'price' : 5
+      'description' : 'A delightful combination of our signature coffee blend and d'
+    },
+    'Fin & Tonic' : {
+      'image' : link,
+      'price' : 12,
+      'description' : text
+    },
+    'Hammertime LIIT' : {
+      'image' : link,
+      'price' : 13,
+      'description' : text
+    },
+    'Blood in the water Mary' : {
+      'image' : link,
+      'price' : 13,
+      'description' : text
+    }
+  }
+}
