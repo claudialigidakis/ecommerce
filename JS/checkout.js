ce54ff8c726caaf46f44324bedd6144541b6e08b
Gavin Look <GTLook@gmail.com>
2018-04-09 21:50:53 -0700

    added forms, form formating, html selectors for info

---
+++ JS/checkout.js
@@ -0,0 +1,14 @@
+//declare HTML form boxes
+const info = document.querySelector('.info')
+const shipingInfo = document.querySelector('.shippingInfo')
+const billingInfo = document.querySelector('.billingInfo')
+
+//declare HTML form links
+const firstName = document.querySelector('.firstName')
+const lastName = document.querySelector('.lastName')
+const email = document.querySelector('.email')
+
+//functions
+const clearValues = () => {
+
+}
