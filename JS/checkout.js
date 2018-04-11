9e59ad07eceeeebe6189935dadb0e6a49100444e
Gavin Look <GTLook@gmail.com>
2018-04-10 21:24:10 -0700

    added more event listners for checkout

--- JS/checkout.js
+++ JS/checkout.js
@@ -7,6 +7,13 @@ const billingInfo = document.querySelector('.billingInfo')
 const firstName = document.querySelector('.firstName')
 const lastName = document.querySelector('.lastName')
 const email = document.querySelector('.email')
+//link arrows
+const infoUp = document.querySelector('.infoUp')
+const infoDown = document.querySelector('.infoDown')
+const shipUp = document.querySelector('.shipUp')
+const shipDown = document.querySelector('.shipDown')
+const billUp = document.querySelector('.billUp')
+const billDown = document.querySelector('.billDown')
 
 //functions
 const validateInfo = (key) => {
@@ -23,3 +30,16 @@ const validateInfo = (key) => {
 firstName.addEventListener('keydown', validateInfo)
 lastName.addEventListener('keydown', validateInfo)
 email.addEventListener('keydown', validateInfo)
+infoDown.addEventListener('click',() => {
+  info.classList.add('d-none')
+  shippingInfo.classList.remove('d-none')
+})
+
+//shipping info listeners
+shipDown.addEventListener('click',() => {
+  shippingInfo.classList.add('d-none')
+  billingInfo.classList.remove('d-none')
+})
+shipUp.addEventListener('click',() => {
+  info.classList.remove('d-none')
+  shippingInfo.classList.add('d-none')
